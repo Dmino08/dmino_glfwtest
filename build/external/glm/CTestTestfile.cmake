0e9d1a15590fe342827a3c3ea4e5535969f76f58
Dmino08 <dmino.2025.main@gmail.com>
2025-05-15 18:34:26 +0300

    Stop tracking build folder and a lot of changings

--- build/external/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/D/C++/CMakeLessons/Gits/GLFWTest/external/glm
-# Build directory: D:/D/C++/CMakeLessons/Gits/GLFWTest/build/external/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
